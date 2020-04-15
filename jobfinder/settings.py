0c431c82ab978166d42ff73bbfa0d882505b82ac
mitchelinaju <mitchelinajuo@gmail.com>
2020-04-15 16:59:03 +0100

    integrated 2

--- jobfinder/settings.py
+++ jobfinder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't5&6gmkt=aom$dw%bk_v9idi2_*9b@)ew(l4qzritrcp0lgen$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
